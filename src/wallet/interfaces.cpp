b8a3e33f277842c27b7ab4af3ce546382b4c36fc
ruiyu <iniruiyu@gmail.com>
2026-03-16 10:57:58 +0800

    [0.1.2-3] Revert missing wallet tip tolerance patch / 回退缺失钱包tip容错补丁

--- src/wallet/interfaces.cpp
+++ src/wallet/interfaces.cpp
@@ -353,10 +353,7 @@ public:
         }
         num_blocks = m_wallet->GetLastBlockHeight();
         block_time = -1;
-        // Older wallet state can temporarily reference a tip hash that is no longer
-        // available in the current in-memory block index. Return an unknown time
-        // instead of letting a NonFatalCheckError escape through the Qt event loop.
-        m_wallet->chain().findBlock(m_wallet->GetLastBlockHash(), FoundBlock().time(block_time));
+        CHECK_NONFATAL(m_wallet->chain().findBlock(m_wallet->GetLastBlockHash(), FoundBlock().time(block_time)));
         tx_status = MakeWalletTxStatus(*m_wallet, mi->second);
         return true;
     }
